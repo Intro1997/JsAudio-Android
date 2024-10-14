7377d8e45c6e47c97fb6fbb43d0783d56da798d9
Intro1997 <765856121@qq.com>
2024-10-14 13:32:59 +0800

    fix: param pass error

--- app/audio/src/main/cpp/cpp_layer/AudioContext/AudioContext.cpp
+++ app/audio/src/main/cpp/cpp_layer/AudioContext/AudioContext.cpp
@@ -8,8 +8,8 @@ using AudioPlayerConfig = js_audio::AudioPlayerConfig;
 std::tuple<uint32_t, const float> GetBaseAudioContextParams() {
   if (auto audio_engine_ptr = AudioEngine::Get().lock()) {
     AudioPlayerConfig player_config = audio_engine_ptr->audio_player_config();
-    return {player_config.sample_rate_milli_hz / 1e3,
-            player_config.num_channels};
+    return {player_config.num_channels,
+            player_config.sample_rate_milli_hz / 1e3};
   }
   return {0, 0};
 }
