63a150eb1f4f2bd16a2381cd0791211c34cea7ad
Intro1997 <765856121@qq.com>
2024-10-19 15:58:25 +0800

    feat: update oscillator node interface

--- js_api_define/OscillatorNode.ts
+++ js_api_define/OscillatorNode.ts
@@ -1,6 +1,28 @@
-interface OscillatorNode extends AudioScheduledSourceNode {
+// NOTE: You should not expect the output of oscillator node 
+// to change based on the channelCount property.
+// "An oscillator is always mono." refers to 
+// https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/webaudio/oscillator_handler.cc;drc=0fba000db58b7966ef2b2e9202dabba44f13d62a;l=34
+
+// wed do not support custom now
+// @ts-ignore
+type OscillatorType = /* "custom" | */ "sawtooth" | "sine" | "square" | "triangle";
+
+interface OscillatorOptions extends AudioNodeOptions {
+  detune?: number;
+  frequency?: number;
+
+  /* we do not support periodicWave */
+  // periodicWave?: PeriodicWave;
+
   // @ts-ignore
-  type: "sawtooth" | "sine" | "square" | "triangle"; // not support custom now
+  type?: OscillatorType;
+}
+
+interface OscillatorNode extends AudioScheduledSourceNode {
+  new(context: BaseAudioContext, options?: OscillatorOptions): OscillatorNode;
+
+  // @ts-ignore
+  type: OscillatorType;
   readonly frequency: AudioParam
   /**
    * max value and min value refers to following link
